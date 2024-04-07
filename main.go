377b707fcfe61f73d3998efa12a012864d2782b5
scoo73r <scorb1@gmail.com>
2024-04-06 21:49:09 -0500

    movemetn works

--- main.go
+++ main.go
@@ -1,17 +1,105 @@
 package main
 
-import "github.com/gen2brain/raylib-go/raylib"
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const (
+	screenWidth  = 1000
+	screenHeight = 480
+)
+
+var (
+	running      = true
+	bkgColor     = rl.NewColor(147, 211, 196, 255)
+	grassSprite  rl.Texture2D
+	playerSprite rl.Texture2D
+
+	playerSrc  rl.Rectangle
+	playerDest rl.Rectangle
+
+	playerSpeed float32 = 3
+	musicPaused bool
+	music       rl.Music
+	cam         rl.Camera2D
+)
+
+func drawscene() {
+	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+}
+
+func input() {
+	if rl.IsKeyDown(rl.KeyE) || rl.IsKeyDown(rl.KeyUp) {
+		playerDest.Y -= playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyDown) {
+		playerDest.Y += playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyLeft) {
+		playerDest.X -= playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyF) || rl.IsKeyDown(rl.KeyRight) {
+		playerDest.X += playerSpeed
+	}
+	if rl.IsKeyPressed(rl.KeyP) {
+		musicPaused = !musicPaused
+	}
+}
+
+func update() {
+	running = !rl.WindowShouldClose()
+
+	rl.UpdateMusicStream(music)
+	if musicPaused {
+		rl.PauseMusicStream(music)
+	} else {
+		rl.ResumeMusicStream(music)
+	}
+
+	cam.Target = rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height)))
+}
+func render() {
+	rl.BeginDrawing()
+	rl.ClearBackground(bkgColor)
+	rl.BeginMode2D(cam)
+	drawscene()
+	rl.EndMode2D()
+	rl.EndDrawing()
+}
+
+func init() {
+	rl.InitWindow(screenWidth, screenHeight, "go-farm")
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
+
+	grassSprite = rl.LoadTexture("Sprites/Tilesets/Grass.png")
+	playerSprite = rl.LoadTexture("Sprites/Characters/BasicCharacterSpritesheet.png")
+
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
+	rl.InitAudioDevice()
+	music = rl.LoadMusicStream("audio/tap-room-rag.mp3")
+	musicPaused = false
+	rl.PlayMusicStream(music)
+
+	cam = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)),
+		rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height))), 0.0, 1.0)
+}
+
+func quit() {
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
+	rl.UnloadMusicStream(music)
+	rl.CloseAudioDevice()
+	rl.CloseWindow()
+}
 
 func main() {
-	screenWidth := float32(800)
-	screenHeight := float32(450)
-	
-	rl.InitWindow(screenWidth, screenHeight, "go-farm")
-	rl.SetConfigFlags(rl.FlagVsyncHint)
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
-		rl.EndDrawing()
+
+	for running {
+		input()
+		update()
+		render()
 	}
 	rl.CloseWindow()
 }
